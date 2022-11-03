6ad7592484e43b77f1b8184136a87b988a9f92a5
Joseph-dias <38766207+Joseph-dias@users.noreply.github.com>
2022-11-03 09:21:01 -0700

    Updating crawing

--- Scraper.py
+++ Scraper.py
@@ -2,22 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 
 class Scraper:
+    TagsToScrape = ['main', 'div', 'section', 'p', 'a', 'span']
+
+    isMatch = False
     myUrl = ''
+    foundUrls = set()  # Holding more urls to visit
     def __init__(self, myurl):
         self.myUrl = myurl
 
-    def scrape(self):
+    def scrape(self, keyword):
         data = requests.get(self.myUrl).text  # Pulling html text
         soup = BeautifulSoup(data, 'lxml')
         title = soup.find('title').text
 
-        toReturn = {title}
+        pageWords = set()
 
-        for div in soup.find('body').find_all(['main', 'div', 'section', 'p', 'a', 'span']):
+        titleWords = title.split(' ')
+        for word in titleWords:  # Splitting title words
+            theWord = ''.join(l for l in word if l.isalnum())
+            pageWords.add(theWord)
+
+        for div in soup.find('body').find_all(self.TagsToScrape, recursive=False):
             words = div.text.split(' ')
             for word in words:
                 FinalWord = ''.join(l for l in word if l.isalnum())
                 if len(FinalWord) > 4:
-                    toReturn.add(FinalWord)
+                    pageWords.add(FinalWord)
+            pageWords.update(self.recurse(div))
 
+        self.isMatch = len([k for k in pageWords if keyword.lower() in k.lower()]) > 0
+
+        for link in soup.find_all('a'):
+            if 'http' in link['href']:
+                self.foundUrls.add(link['href'])
+
+    def recurse(self, div):
+        toReturn = set()
+
+        for myDiv in div.find_all(self.TagsToScrape, recursive=False):
+            words = myDiv.text.split(' ')
+            for word in words:
+                FinalWord = ''.join(l for l in word if l.isalnum())
+                if len(FinalWord) > 4:
+                    toReturn.add(FinalWord)
+            toReturn.update(self.recurse(myDiv))
         return toReturn
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from Scraper import Scraper
+from collections import deque
 
 class Starter:
 
+    matching = set()
+    queuedLinks = set()
+    crawlPath = deque()  # Queue of links to crawl over
+
     def Start(self):
+        # Scraping first site
         url = input('Starter Site: ')
-        dataScraper = Scraper(url)
-        print(dataScraper.scrape())
+        keyword = input('Search: ')
+
+        print('')
+        print('Matches:')
+
+        keepCrawling = True
+        while keepCrawling:
+            dataScraper = Scraper(url)
+            dataScraper.scrape(keyword)
+            if dataScraper.isMatch:  # Add link to output if keyword found
+                self.matching.add(url)
+                print(url)
+            self.crawlPath.extend([u for u in dataScraper.foundUrls if u not in self.queuedLinks])
+            self.queuedLinks.update(dataScraper.foundUrls)
+            if len(self.crawlPath) == 0:
+                keepCrawling = False
+            else:
+                url = self.crawlPath.popleft()
+        if len(self.matching) == 0:
+            print('NOTHING FOUND')
 
 
 
-Starter().Start()
+program = Starter()
+
+program.Start()
